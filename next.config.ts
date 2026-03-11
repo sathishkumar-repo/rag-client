304b17e92be1cb8e839dbc717bee40ddadc264f6
Sathish Kumar <sathishkumar.balraj@gmail.com>
2026-03-11 11:21:05 +0530

    ready for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-} as NextConfig;
+};
 
 export default nextConfig;
